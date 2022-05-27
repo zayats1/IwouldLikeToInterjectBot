4bf4301a3e947ab73244cef24fd59aee6f77fe17
zayats1 <zaiats.bogdan@gmail.com>
2022-05-27 12:42:00 +0300

    It works

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,32 +2,38 @@ import dev.kord.core.Kord
 import dev.kord.core.entity.ReactionEmoji
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
-import dev.kord.gateway.Intent
-import dev.kord.gateway.PrivilegedIntent
-import kotlinx.coroutines.delay
+
+const val LINUX = "Linux"
+const val I_WOULD_LIKE_TO_INTERJECT = "I'd just like to interject for a moment"
+
+const val PENGUIN_EMOJI = "\uD83D\uDC27"
+const val GNU_EMOJI = "\uD83D\uDC03"
+
+fun toEmoji(unicode: String) = ReactionEmoji.Unicode(unicode)
+
+fun isFound(input: String, expected: String): Boolean {
+    val expectedRegex = expected.toRegex(RegexOption.IGNORE_CASE)
+    expectedRegex.find(input) ?: return false
+    return true
+}
 
 suspend fun main() {
-    println("IWouldLikeToInterjectBot")
+    val bot = Kord(ClassLoader.getSystemResource(".botToken").readText())
 
-    val kord = Kord(ClassLoader.getSystemResource(".botToken").readText())
-    val pingPong = ReactionEmoji.Unicode("\uD83C\uDFD3")
+    bot.on<MessageCreateEvent> {
+        val content = message.content
 
-    kord.on<MessageCreateEvent> {
-        val linux = "linux".toRegex(RegexOption.IGNORE_CASE)
-
-        val interject = "I'd just like to interject for a moment".toRegex(RegexOption.IGNORE_CASE)
-        val match2 =   interject.find(message.content)
-
-        if (match2 != null){
+        if (isFound(content, I_WOULD_LIKE_TO_INTERJECT)) {
             return@on
         }
-
-        val match = linux.find(message.content) ?: return@on
-
-        val response = message.channel.createMessage(ClassLoader.getSystemResource("MyResponse.txt").readText())
-        response.addReaction(pingPong)
+        if (isFound(content, LINUX)) {
+            val response = message.channel.createMessage(ClassLoader.getSystemResource("MyResponse.txt").readText())
+            message.addReaction(toEmoji(PENGUIN_EMOJI))
+            message.channel.createMessage(GNU_EMOJI)
+        }
+        return@on
     }
 
-    kord.login()
+    bot.login()
 }
 
